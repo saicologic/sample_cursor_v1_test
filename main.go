3c755a72fd944ef46aa9e92982a5cec1ced9ebdf
Satoru Mikami <saicologic@gmail.com>
2025-06-05 11:18:49 +0900

    First commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func main() {
+	var x, y int
+	fmt.Print("1つ目の数字を入力してください: ")
+	fmt.Scan(&x)
+	fmt.Print("2つ目の数字を入力してください: ")
+	fmt.Scan(&y)
+	sum := add(x, y)
+	fmt.Printf("合計: %d\n", sum)
+}
